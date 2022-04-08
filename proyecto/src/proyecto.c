fb4da5387f76c41b05d5b5326136ea511274f1de
carrilloFN <fcarrillo@frba.utn.edu.ar>
2022-04-08 17:55:44 -0300

    Ejercicio1 completo

--- proyecto/src/proyecto.c
+++ proyecto/src/proyecto.c
@@ -10,8 +10,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* string_concat(const char* cad1, const char* cad2){
+	char* cadena = malloc(strlen(cad1)+strlen(cad2)+1);
+	strcpy(cadena,cad1);
+	strcat(cadena,cad2);
+	return cadena;
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	char* nombre="Carri";
+	char* saludo = string_concat("Hola ",nombre);
+	printf("%s",saludo);
+	free(saludo);
 	return EXIT_SUCCESS;
 }
+
+
