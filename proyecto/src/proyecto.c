d6fb83e013d642b02a84c2e41cfeadf58a190917
carrilloFN <fcarrillo@frba.utn.edu.ar>
2022-04-08 19:04:33 -0300

    Ejercicio 2

--- proyecto/src/proyecto.c
+++ proyecto/src/proyecto.c
@@ -12,19 +12,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* string_concat(const char* cad1, const char* cad2){
-	char* cadena = malloc(strlen(cad1)+strlen(cad2)+1);
-	strcpy(cadena,cad1);
-	strcat(cadena,cad2);
-	return cadena;
+//Recibe dos punteros constantes a char
+//Devuelve puntero a char que almacena la concatenacion
+char* string_concat(const char* str1, const char* str2){
+	//Declaro un puntero a char y le preparo el espacio en memoria.
+	char* str = malloc(strlen(str1)+strlen(str2)+1);
+	strcpy(str,str1);
+	strcat(str,str2);
+	return str;
 }
 
-int main(void) {
+//Recibe dos punteros constantes a char y la direccion de memoria de un puntero.
+//Al finalizar, el puntero contendra la concatenacion.
+void string_concat_dinamyc(const char* str1,const char* str2, char** str){
+	*str = malloc(strlen(str1)+strlen(str2)+1);
+	strcpy(*str,str1);
+	strcat(*str,str2);
+}
+
+/*
+
+Ejercicio1
+
+int main(void){
 	char* nombre="Carri";
 	char* saludo = string_concat("Hola ",nombre);
 	printf("%s",saludo);
 	free(saludo);
 	return EXIT_SUCCESS;
 }
+*/
 
+int main(){
+	char* nombre="Carri";
+	char* saludo;
+	string_concat_dinamyc("Hola ",nombre,&saludo);
+	printf("%s",saludo);
+	free(saludo);
+}
 
