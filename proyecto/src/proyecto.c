ca29839869d709bf363885563fcfc0ed40b5967c
carrilloFN <fcarrillo@frba.utn.edu.ar>
2022-04-08 21:51:56 -0300

    Ejercicios C Basicos con String

--- proyecto/src/proyecto.c
+++ proyecto/src/proyecto.c
@@ -12,11 +12,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef char* string;
+
 //Recibe dos punteros constantes a char
 //Devuelve puntero a char que almacena la concatenacion
-char* string_concat(const char* str1, const char* str2){
+char* string_concat(const string str1, const string str2){
 	//Declaro un puntero a char y le preparo el espacio en memoria.
-	char* str = malloc(strlen(str1)+strlen(str2)+1);
+	string str = malloc(strlen(str1)+strlen(str2)+1);
 	strcpy(str,str1);
 	strcat(str,str2);
 	return str;
@@ -24,7 +26,7 @@ char* string_concat(const char* str1, const char* str2){
 
 //Recibe dos punteros constantes a char y la direccion de memoria de un puntero.
 //Al finalizar, el puntero contendra la concatenacion.
-void string_concat_dinamyc(const char* str1,const char* str2, char** str){
+void string_concat_dinamyc(const string str1,const string str2, string* str){
 	*str = malloc(strlen(str1)+strlen(str2)+1);
 	strcpy(*str,str1);
 	strcat(*str,str2);
@@ -32,8 +34,8 @@ void string_concat_dinamyc(const char* str1,const char* str2, char** str){
 
 //Recibe un puntero a char con el mail completo, y la direccion de memoria de dos punteros donde se guardará
 // el usuario y el dominio del mail.
-void mail_split(const char* mail, char** user, char** dominio){
-	char* correoParticionado = malloc(strlen(mail)+1);
+void mail_split(const string mail, string* user, string* dominio){
+	string correoParticionado = malloc(strlen(mail)+1);
 	strcpy(correoParticionado,mail);
 	strtok(correoParticionado,"@");
 	*user = malloc(strlen(correoParticionado)+1);
@@ -49,8 +51,8 @@ void mail_split(const char* mail, char** user, char** dominio){
 Ejercicio1
 
 int main(void){
-	char* nombre="Carri";
-	char* saludo = string_concat("Hola ",nombre);
+	string nombre="Carri";
+	string saludo = string_concat("Hola ",nombre);
 	printf("%s",saludo);
 	free(saludo);
 	return EXIT_SUCCESS;
@@ -62,8 +64,8 @@ int main(void){
 Ejercicio2
 
 int main(void){
-	char* nombre="Carri";
-	char* saludo;
+	string nombre="Carri";
+	string saludo;
 	string_concat_dinamyc("Hola ",nombre,&saludo);
 	printf("%s",saludo);
 	free(saludo);
@@ -71,9 +73,9 @@ int main(void){
 */
 
 int main(void){
-	char* mail = "franco.carrillo@utn.edu.com.ar";
-	char* user;
-	char* dominio;
+	string mail = "franco.carrillo@utn.edu.com.ar";
+	string user;
+	string dominio;
 	mail_split(mail,&user,&dominio);
 	printf("User: %s\n",user);
 	printf("Domain: %s\n",dominio);
